19b3492b48cf358454696ca898599dfd0b6e5b2f
Jonathan Frederickson <jfrederickson@linode.com>
2017-08-18 09:58:17 -0400

    emacs: Change theme and switch to bar cursor

--- emacs/.emacs.d/theme.el
+++ emacs/.emacs.d/theme.el
@@ -4,7 +4,10 @@
 
 ;; Theme
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-(load-theme 'material)
+;(load-theme 'material)
+(load-theme 'gruvbox)
 
 ;; Don't show the startup message when emacs starts
 (setq inhibit-startup-message t)
+
+(setq-default cursor-type 'bar)
