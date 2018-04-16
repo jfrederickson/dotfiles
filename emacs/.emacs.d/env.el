d2094d80da33b6c744ef08693548f6bd63683a68
Jonathan Frederickson <jfrederickson@linode.com>
2018-04-16 15:57:31 -0400

    Update emacs env config

--- emacs/.emacs.d/env.el
+++ emacs/.emacs.d/env.el
@@ -10,7 +10,24 @@
 (require 'better-defaults)
 (require 'mingus-stays-home)
 
+;; Enable project awareness
 (projectile-mode)
+
+;; Enable my common docsets in helm-dash
+(setq helm-dash-docsets '("SaltStack" "Kubernetes"))
+
+;; Set up org-mode the way I want it
+(setq org-directory "~/org")
+(setq org-agenda-files (mapcar (lambda (x) (concat org-directory x)) '("/inbox.org" "/gtd-active.org")))
+(setq org-refiles-jfred (mapcar (lambda (x) (concat org-directory x)) '("/inbox.org" "/gtd-active.org" "/gtd-next.org" "/gtd-someday.org" "/gtd-complete.org")))
+(setq org-refile-targets '((org-refiles-jfred :maxlevel . 1)))
+(setq org-outline-path-complete-in-steps nil)
+(setq org-default-notes-file (concat org-directory "/inbox.org"))
+(setq org-capture-templates
+      '(("t" "Todo" entry (file "~/org/inbox.org")
+         "* TODO %?\n  %a")
+        ("j" "Journal" entry (file+olp+datetree "~/org/journal.org")
+         "* %?\nEntered on %U\n  %i\n  %a")))
 
 ;; Window transparency
 (set-frame-parameter (selected-frame) 'alpha '(90 . 90))
