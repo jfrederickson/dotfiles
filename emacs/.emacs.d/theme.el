ea063fad249a5542e2466c14e4cdbbb93d9d6c8e
Jonathan Frederickson <jfrederickson@linode.com>
2018-08-21 15:10:33 -0400

    theme.el: Add a few other themes I like (commented out)

--- emacs/.emacs.d/theme.el
+++ emacs/.emacs.d/theme.el
@@ -4,8 +4,14 @@
 
 ;; Theme
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-;(load-theme 'material)
+;;(load-theme 'material)
 (load-theme 'gruvbox)
+;;(load-theme 'flatui)
+;;(load-theme 'tron)
+
+
+;;(require 'moe-theme)
+;;(require 'moe-theme-switcher)
 
 ;; Don't show the startup message when emacs starts
 (setq inhibit-startup-message t)
