ffa61b2d1560eb6cdb3d924bf44a4d71b2124fc5
Jonathan Frederickson <jonathan@terracrypt.net>
2019-06-02 17:32:39 -0400

    Add guix-packages to debbugs list

--- emacs/.emacs.d/env.el
+++ emacs/.emacs.d/env.el
@@ -49,3 +49,5 @@
   "Sets the transparency of the frame window. 0=transparent/100=opaque"
   (interactive "nTransparency Value 0 - 100 opaque:")
   (set-frame-parameter (selected-frame) 'alpha value))
+
+;;(add-to-list 'debbugs-gnu-all-packages "guix-patches")
