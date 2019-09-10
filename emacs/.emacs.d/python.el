9a747b900cf2d763fbfaafd1341754f9c26e8b06
Jonathan Frederickson <jfrederickson@linode.com>
2019-09-10 09:46:52 -0400

    emacs: Current python config

--- emacs/.emacs.d/python.el
+++ emacs/.emacs.d/python.el
@@ -1,7 +1,8 @@
-(setq python-shell-interpreter "/usr/bin/python")
+;;(setq python-shell-interpreter "/usr/bin/python")
 (setq jedi:environment-virtualenv
       (list "virtualenv" "--system-site-packages"))
 
 (add-hook 'python-mode-hook 'jedi:setup)
+(elpy-enable)
 (setq jedi:setup-keys t)                      ; optional
 (setq jedi:complete-on-dot t)                 ; optional
