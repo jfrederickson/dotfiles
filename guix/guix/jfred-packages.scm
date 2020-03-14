627a03fcd2f6c6c82d576c1a5fd2c771690de50d
Jonathan Frederickson <jonathan@terracrypt.net>
2020-03-14 15:39:08 -0400

    guix: Factor out linode-api4 package definition into another file

---
+++ guix/guix/jfred-packages.scm
@@ -0,0 +1,38 @@
+(define-public jfred:python-linode-api4
+  (package
+    (name "python-linode-api4")
+    (version "2.3.3")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/linode/linode_api4-python.git")
+             (commit version)))
+       (sha256
+        (base32
+         "09j5ym7k543mz1w733zib6289xddsla6w9xyriwy58mbrk9b7623"))))
+    (build-system python-build-system)
+    (arguments
+     `(#:phases
+       (modify-phases %standard-phases
+         ;;(replace 'check
+         ;;  (lambda _
+         ;;    (invoke "tox"))))))
+         (delete 'check))))
+    ;;(native-inputs
+    ;; `(("python-mock" ,python-mock)
+    ;;   ("python-tox" ,python-tox)
+    ;;   ("python-pytest" ,python-pytest)
+    ;;   ("python-coverage" ,python-coverage)
+    ;;   ("python-pytest-mock" ,python-pytest-mock)
+    ;;   ("python-pylint" ,python-pylint)))
+    (propagated-inputs
+     `(("python-future" ,python-future)
+       ("python-requests" ,python-requests)))
+    (home-page
+     "https://github.com/linode/linode_api4-python")
+    (synopsis
+     "The official python SDK for Linode API v4")
+    (description
+     "The official python SDK for Linode API v4")
+    (license license:bsd-3)))
--- guix/guix/manifest.scm
+++ guix/guix/manifest.scm
@@ -8,47 +8,10 @@
  (gnu packages python-web)
  ((guix licenses) #:prefix license:))
 
-(define-public local:python-linode-api4
-  (package
-    (name "python-linode-api4")
-    (version "2.3.3")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://github.com/linode/linode_api4-python.git")
-             (commit version)))
-       (sha256
-        (base32
-         "09j5ym7k543mz1w733zib6289xddsla6w9xyriwy58mbrk9b7623"))))
-    (build-system python-build-system)
-    (arguments
-     `(#:phases
-       (modify-phases %standard-phases
-         ;;(replace 'check
-         ;;  (lambda _
-         ;;    (invoke "tox"))))))
-         (delete 'check))))
-    ;;(native-inputs
-    ;; `(("python-mock" ,python-mock)
-    ;;   ("python-tox" ,python-tox)
-    ;;   ("python-pytest" ,python-pytest)
-    ;;   ("python-coverage" ,python-coverage)
-    ;;   ("python-pytest-mock" ,python-pytest-mock)
-    ;;   ("python-pylint" ,python-pylint)))
-    (propagated-inputs
-     `(("python-future" ,python-future)
-       ("python-requests" ,python-requests)))
-    (home-page
-     "https://github.com/linode/linode_api4-python")
-    (synopsis
-     "The official python SDK for Linode API v4")
-    (description
-     "The official python SDK for Linode API v4")
-    (license license:bsd-3)))
+(include "jfred-packages.scm")
 
 (packages->manifest
- (append `(,local:python-linode-api4)
+ (append `(,jfred:python-linode-api4)
          (map (compose list specification->package+output)
               '("glibc-utf8-locales"
                 "gs-fonts"
