4ed672adf06cec205b32300dfd2ee5163c291bd2
Jonathan Frederickson <jfrederickson@linode.com>
2020-10-13 13:40:54 -0400

    nav.el: Add new utility functions to config
    Haven't pushed my config up in a while; here's some newer utility
    functions from my config that have been added over the last few
    months.

--- emacs/.emacs.d/nav.el
+++ emacs/.emacs.d/nav.el
@@ -24,6 +24,97 @@
 	  (select-window first-win)
 	  (if this-win-2nd (other-window 1))))))
 
+(defun yank-with-indent ()
+  (interactive)
+  (let ((indent
+         (buffer-substring-no-properties (line-beginning-position) (line-end-position))))
+    (message indent)
+    (yank)
+    (narrow-to-region (mark t) (point))
+    (pop-to-mark-command)
+    (replace-string "\n" (concat "\n" indent))
+    (widen)))
+
+;;; Stefan Monnier <foo at acm.org>. It is the opposite of 
+;;; fill-paragraph. Takes a multi-line paragraph and makes 
+;;; it into a single line of text.
+(defun unfill-paragraph ()
+  (interactive)
+  (let ((fill-column (point-max)))
+    (fill-paragraph nil)))
+
+;; Lines to comma-separated list, from https://stackoverflow.com/questions/17922208/emacs-convert-items-on-separate-lines-to-a-comma-separated-list/26433323
+(defun lines-to-cslist (start end &optional arg)
+  (interactive "r\nP")
+  (let ((insertion
+         (mapconcat 
+          (lambda (x) (format "%s" x))
+          (split-string (buffer-substring start end)) ", ")))
+    (delete-region start end)
+    (insert insertion)
+    (when arg (forward-char (length insertion)))))
+
+;; Get a block of text at the indentation level of the first
+;; line. Useful for e.g. running commands on indented YAML strings
+(defun get-indented-text (start end)
+  (let ((linestart
+         (save-excursion
+           (goto-char start)
+           (move-beginning-of-line nil)))
+        (lineend
+         (save-excursion
+           (goto-char end)
+           (move-end-of-line nil)
+           (point)))
+        (indentlevel
+         (save-excursion
+           (goto-char start)
+           (back-to-indentation)
+           (current-column))))
+    (save-excursion
+      (let ((buf (make-temp-name "indentcopy")))
+        (copy-to-buffer buf linestart lineend)
+        (set-buffer buf)
+        (let ((rectend
+               (save-excursion
+                 (goto-char lineend)
+                 (move-beginning-of-line nil)
+                 (forward-char indentlevel)
+                 (point))))
+          (kill-rectangle (point-min) rectend)
+          (let ((result (buffer-substring (point-min) (point-max))))
+            (kill-buffer buf)
+            result))))))
+
+(defun gpg-analyze (start end)
+  (interactive "r")
+  (if (use-region-p)
+      (save-excursion
+        (let ((encrypted (get-indented-text start end)))
+          (with-temp-buffer
+            (insert encrypted)
+            (shell-command-on-region (point-min) (point-max) "gpg --list-packets"))))))
+
+(defun ssl-analyze (start end)
+  (interactive "r")
+  (if (use-region-p)
+      (save-excursion
+        (let ((cert (get-indented-text start end)))
+          (with-temp-buffer
+            (insert cert)
+            (shell-command-on-region (point-min) (point-max) "openssl x509 -text -noout"))))))
+
+(defun interactive-get-indent (start end)
+  (interactive "r")
+  (if (use-region-p)
+      (message (get-indented-text start end))))
+
+;; Accept prefix arg to avoid linebreaks in base64-encode-region
+(defun base64-encode-region-prefix-arg (&rest _args)
+  "Pass prefix arg as third arg to `base64-encode-region'."
+  (interactive "r\nP"))
+(advice-add 'base64-encode-region :before #'base64-encode-region-prefix-arg)
+
 (define-key ctl-x-4-map "t" 'toggle-window-split)
 (global-set-key (kbd "C-x |") 'toggle-window-split)
 
