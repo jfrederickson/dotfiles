392b0cbf85d6faf8b1a20e72bc47e8586f4a1819
Jonathan Frederickson <jfrederi@akamai.com>
2024-02-06 16:53:41 -0500

    Move emacs package management into Guix
    The long startup times (I think due to `(package-initialize)`) had
    been bugging me, so I finally took some time to move my emacs config
    into Guix and out of emacs package management tooling. Also switches
    to the cyberpunk theme because I was looking for a change anyway and
    saw this first in Guix :)

---
+++ guix/guix/home/configs/emacs.scm
@@ -0,0 +1,22 @@
+(define-module (home configs emacs)
+  #:use-module (gnu packages))
+
+(define-public %emacs-packages
+  (map (compose list specification->package+output)
+       (list "emacs-git-link"
+             "emacs-json-mode"
+             "emacs-groovy-modes"
+             "emacs-jenkinsfile-mode"
+             "emacs-jabber"
+             "emacs-magit"
+             "emacs-all-the-icons"
+             "emacs-ag"
+             "emacs-editorconfig"
+             "emacs-nginx-mode"
+             "emacs-visual-fill-column"
+             "emacs-dockerfile-mode"
+             "emacs-expand-region"
+             "emacs-helm"
+             "emacs-cyberpunk-theme")))
+
+
