69830053b4a161e201d8533e2f7edded158c1b8b
Jonathan Frederickson <jfrederi@akamai.com>
2024-05-01 15:45:10 -0400

    Switch to private dotfiles for work machine config

--- guix/guix/home/machines/work.scm
+++ guix/guix/home/machines/work.scm
@@ -1,9 +1,1 @@
-(define-module (home machines work)
-  #:use-module (gnu home)
-  #:use-module (home configs emacs)
-  #:use-module (home machines base))
-
-(home-environment
- (packages
-  (append %jfred-packages %emacs-packages))
- (services %jfred-services))
+(load (format #f "~a/.dotfiles-work/guix/home/machines/work.scm" (getenv "HOME")))
