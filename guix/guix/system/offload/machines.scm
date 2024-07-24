6c403fb4df798e128cf213e5185704e6e07106b8
Jonathan Frederickson <jonathan@terracrypt.net>
2024-07-23 21:23:45 -0400

    Add machines.scm

---
+++ guix/guix/system/offload/machines.scm
@@ -0,0 +1,5 @@
+(list (build-machine
+       (name "10.4.42.237")
+       (systems (list "x86_64-linux" "i686-linux"))
+       (host-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO2YQajUExNyT6WWtnOg784/aihdwMnRYoOxz/jEHSQi")
+       (user "jfred")))
